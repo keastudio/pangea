97a394ee8f4b93bc93b313d806eb6208826ece74
Jake Fulton <jake@jakefulton.com>
2022-08-27 02:31:42 +1200

    Minor fix to image optimization script

--- www/src/utils/optimize-images.ts
+++ www/src/utils/optimize-images.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-run --allow-read
+#!/usr/bin/env -S deno run --allow-run --allow-read --allow-write
 
 import * as Colors from 'https://deno.land/std@0.152.0/fmt/colors.ts'
 import { ensureDir } from 'https://deno.land/std@0.152.0/fs/mod.ts'
@@ -8,7 +8,7 @@ try {
   Deno.run({ cmd: ['convert', '-version'] })
 } catch (error) {
   if (error instanceof Deno.errors.NotFound) {
-    console.error(Colors.red(`The program 'convert' is not in your PATH. Please install imagemagick.`))
+    console.error(Colors.red(`The program 'convert' is not in your PATH. Please install 'imagemagick'.`))
     Deno.exit(1)
   }
 }
@@ -23,7 +23,7 @@ try {
   }
 } catch (error) {
   if (error instanceof Deno.errors.NotFound) {
-    console.warn(Colors.yellow(`Warning, the program 'avifenc' is not in your PATH. Please install 'libavif'. Falling back to imagemagick.`))
+    console.warn(Colors.yellow(`Warning, the program 'avifenc' is not in your PATH. Please install 'libavif'. Falling back to 'imagemagick'.`))
   }
   libavifIsInstalled = false
 }
@@ -37,17 +37,11 @@ const results = await Promise.all(
     .map(
       ({ name }) => ['avif', 'webp', 'jpg'].map(
         format => imageWidths.map(
-          width => [
-            { name, width, format },
-            { name, width, format },
-            { name, width, format },
-            { name, width, format },
-            { name, width, format }
-          ]
+          width => ({ name, width, format })
         ) 
       )
     )
-    .flat(3)
+    .flat(2)
     .map(async ({ name, width, format }) => {
       const command = (name === 'avif' && libavifIsInstalled === true)
         ? `avifenc ./src/static/images/original/${name} ./src/static/images/optimized/${name.split('.')[0]}_w${width}.${format}`
