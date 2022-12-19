b6ced5b96f9fbe99c0c35b14423ec20c4a8ccacb
Jake Fulton <jake@jakefulton.com>
2022-12-19 21:36:08 +1300

    Fix netlify function which was erroring for an unknown reason, by changing from a redirect to a proxy

--- www/netlify/edge-functions/homepage.ts
+++ www/netlify/edge-functions/homepage.ts
@@ -5,8 +5,16 @@ export default async (request: Request, context: Context) => {
 
   const response = await context.next()
 
-  if (userAgent?.includes('Deno')) {
-    return Response.redirect('https://deno.land/x/pangea/init.ts')
+  if (userAgent?.includes('Deno')) {    
+    const response = await fetch('https://deno.land/x/pangea/init.ts')
+
+    if (!response.ok) {
+      console.error(response.status)
+    }
+
+    const text = await response.text()
+
+    return context.json(text)
   } else {
     return response
   }
